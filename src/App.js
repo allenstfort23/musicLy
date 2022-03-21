cb837d85e46220c5d66fad3ff01b8fecf1594635
Allen St Fort <allenstfort1@gmail.com>
2022-03-21 00:28:12 -0400

    Added hero banner and picture

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ import Login from "./pages/login";
 import Signup from "./pages/signup";
 import About from "./pages/about";
 import Dashboard from "./pages/dashboard";
-import useAuth from "./useAuth";
 import Error from "./pages/error";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import injectContext from "./store/appContext";
 
-const code = new URLSearchParams(window.location.search).get("code");
-function App() {
-  return code ? <Dashboard code={code} /> : <Login />;
-  // <Router>
-  //   <Menu />
-  //   <Routes>
-  //     <Route path="/" element={<Home />} />
-  //     <Route path="/login" element={<Login />} />
-  //     <Route path="/dashboard" element={<Dashboard />} />
-  //     <Route path="/signup" element={<Signup />} />
-  //     <Route path="/about" element={<About />} />
-  //     <Route path="*" element={<Error />} />
-  //   </Routes>
-  // </Router>
+// const code = new URLSearchParams(window.location.search).get("code");
+export default function App() {
+  const code = new URLSearchParams(window.location.search).get("code");
+  code ? <Dashboard code={code} /> : <Login />;
+
+  return (
+    <Router>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,16 +1,72 @@
 import React from "react";
-import Music from "./img/music-banner.jpeg";
+import MockUp from "./img/Mock up.png";
 import "./banner.css";
 
 function Banner() {
   return (
-    <header className="banner-layout">
-      <img className="banner" style={{ backgroundSize: "cover" }} src={Music} />
-      <h1 className="banner__title">MusicLy</h1>
-      <h2 className="banner__text">All Your Music In One Place</h2>
-      <div className="banner__fadeBottom text-center">Scroll To The Right</div>
-    </header>
+    <div className="static-slider9 position-relative">
+      <div className="row">
+        <div className="container">
+          <div className="col-md-5 info-detail align-self-center">
+            <h1 className="title">MusicLy</h1>
+            <h6 className="subtitle font-weight-normal">
+              All Your Music In One Place. While Your On The Go!
+            </h6>
+            <p className="text-dark font-weight-medium">
+              Don't have an account. Sign up, it's for free.
+            </p>
+            <form className="form-inline">
+              <div className="form-group">
+                <label for="inputPassword2" className="sr-only">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control form-control-lg mr-2 mb-2"
+                  id="inputPassword2"
+                  placeholder="email address"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-md btn-success-gradiant text-white border-0 mb-2"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+        <div
+          className="col-md-5 bg-img"
+          style={{
+            backgroundImage: `url("https://lemonade.co/wp-content/uploads/2018/06/shutterstock_438996334.jpg")`,
+          }}
+        ></div>
+      </div>
+    </div>
   );
 }
 
 export default Banner;
+
+{
+  /* <div classNameName="banner-layout">
+  <div classNameName="row">
+    <div classNameName="container">
+      <div className="col-md-5 info-detail align-self-center">
+        <h1 className="title">MusicLy</h1>
+        <h6 className="subtitle font-weight-normal">
+          All Your Music In One Place. While Your On The Go!
+        </h6>
+        <p className="text-dark font-weight-medium">Don't have an account?</p>
+        <button
+          type="submit"
+          className="btn btn-md btn-success-gradiant text-white border-0 mb-2"
+        >
+          Sign Up
+        </button>
+      </div>
+    </div>
+  </div>
+</div>; */
+}
